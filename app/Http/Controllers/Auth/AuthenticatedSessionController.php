66508c0d9b55afce9d6d372090308ca464fef337
Dale Quist <enablemediahub2024@gmail.com>
2026-09-20 22:01:59 +0100

    Send admins directly to settings

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,7 +39,7 @@ class AuthenticatedSessionController extends Controller
 
         $user = $request->user();
         if ($user?->role === 'admin' && $user->tenant_id !== null) {
-            return redirect()->intended(route('tenant.products.index', ['tenant' => $user->tenant_id], absolute: false));
+            return redirect(route('tenant.products.index', ['tenant' => $user->tenant_id], absolute: false));
         }
 
         return redirect()->intended(route('dashboard', absolute: false));
